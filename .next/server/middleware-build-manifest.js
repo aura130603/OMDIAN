60dafc64c5025d28f4c8ed423f4d7e64c778da89
aura130603 <auramaharani714@gmail.com>
2025-09-11 14:54:56 +0700

    beneran hosting besok sabtu bismillah

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -20,15 +20,25 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/dashboard": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/dashboard.js"
+    ],
+    "/employee-management": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/employee-management.js"
+    ],
     "/login": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/login.js"
     ],
-    "/register": [
+    "/training-history": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/register.js"
+      "static/chunks/pages/training-history.js"
     ]
   },
   "ampFirstPages": []
